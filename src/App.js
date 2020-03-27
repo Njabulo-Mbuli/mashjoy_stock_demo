4f22eb2287e0ba2ad4cf890300a277738305fd97
Njabulo-Mbuli <hopewell.mbuli@gmail.com>
2020-03-27 16:43:49 +0200

    Added first cars

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { withRouter } from 'react-router-dom'
 
-function App() {
+const useStyles = makeStyles({
+  spacer: {
+    height: 65,
+  },
+  card: {
+    boxSizing: "border-box",
+    border: "2px solid rgba(0,0,0,0)",
+    '&:hover': {
+      cursor: "pointer",
+      border: "2px solid black",
+    },
+    maxWidth: 200,
+  },
+});
+
+const App = () => {
+  const handleItemClick = () =>{
+    console.log("item was clicked")
+  }
+  
+  let classes = useStyles()
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card className={classes.card} onClick={() => { handleItemClick("ALT-VLB-N-3XL") }}>
+        <CardActionArea className="cardImage">
+          <CardMedia component="img" height="auto" title="ALT-VLB-RB-MOFR" image={`https://productcatalogue2020.s3.amazonaws.com/ALT-VLB-RB-MOFR_460_350.jpg`} />
+        </CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Ladies ¾ Sleeve Viscount Shirt
+      </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            R 189.99
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   );
 }
